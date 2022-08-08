8393bb4ad5356aea02933501466275a662cc48f8
GEORGI MALKHASYAN <malkhg2@medtronic.com>
2022-08-08 11:07:21 -0700

    Initial Commit

---
+++ ISS_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ISS_App
+//
+//  Created by Malkhasyan, Georgi on 8/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
